a277f299137264bf4c99b3db4b60777a141d449a
Nico <nico@MacBook-Pro.localdomain>
2019-08-08 18:13:30 +0200

    Updates

---
+++ precache-manifest.3a9c73718b00980dd71d768d450dfaf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bf5c458ec1842f5e2606204464de92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df48a22205c2bea5647d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "df48a22205c2bea5647d",
+    "url": "/static/js/main.3c2a1d9d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
